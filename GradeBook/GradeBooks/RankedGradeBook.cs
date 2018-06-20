b402f93fe966e38143657d8b3262938f76bc57a4
Jaydon Sunley <Jaydon.Sunley@myob.com>
2018-06-20 18:48:38 +1200

    setup override for grades

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GradeBook.GradeBooks
@@ -9,6 +10,24 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = Enums.GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException("Ranked-frading requires a minimum of 5 students to work");
+            var threshhold = (int)Math.Ceiling(Students.Count * 0.2);
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+            if (grades[threshhold - 1] <= averageGrade)
+                return 'A';
+            else if (grades[(threshhold * 2) - 1] <= averageGrade)
+                return 'B';
+            else if (grades[(threshhold * 3) - 1] <= averageGrade)
+                return 'C';
+            else if (grades[(threshhold * 4) - 1] <= averageGrade)
+                return 'D';
+            else
+                return 'F';
         }
     }
 }
